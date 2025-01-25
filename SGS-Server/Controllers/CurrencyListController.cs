69067fc62b3c185c1dcda1b1463199222c81c967
PavelVasilchikov <91344984+PavelVasilchikov@users.noreply.github.com>
2025-01-25 19:28:36 +0300

    Создание контроллера для API

--- SGS-Server/Controllers/CurrencyListController.cs
+++ SGS-Server/Controllers/CurrencyListController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SGS_Server.Models;
+using SGS_Server.Services;
 
 namespace SGS_Server.Controllers
 {
@@ -7,6 +8,25 @@ namespace SGS_Server.Controllers
     [Route("[controller]")]
     public class CurrencyListController : ControllerBase
     {
-        
+        private readonly CurrencyService _currencyService;
+
+        public CurrencyListController(CurrencyService currencyService)
+        {
+            _currencyService = currencyService;
+        }
+
+        [HttpGet("currencies")]
+        public async Task<IActionResult> GetCurrencies(int page = 1 , int pageSize = 5)
+        {
+            var currencies = await _currencyService.GetCurrenciesAsync((page - 1) * pageSize, pageSize);
+            return Ok(currencies);
+        }
+
+        [HttpGet("currency/{charCode}")]
+        public async Task<IActionResult> GetCurrency(string charCode)
+        {
+            var currency = await _currencyService.GetCurrencyAsync(charCode);
+            return currency != null ? Ok(currency) : NotFound();
+        }
     }
 }
