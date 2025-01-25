3d8540308ae132ecd01fb9bd16311cc0555d1d0a
PavelVasilchikov <91344984+PavelVasilchikov@users.noreply.github.com>
2025-01-25 17:53:05 +0300

    Создание проекта ASP с пустыми моделями и контроллером

---
+++ SGS-Server/Controllers/CurrencyListController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using SGS_Server.Models;
+
+namespace SGS_Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyListController : ControllerBase
+    {
+        
+    }
+}
---
+++ SGS-Server/Models/CurrencyListModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace SGS_Server.Models
+{
+    public class CurrencyListModel
+    {
+        
+    }
+}
