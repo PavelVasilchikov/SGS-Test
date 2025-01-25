36c0158c96ff8ab077fa55ba5fd9eff09269fa3c
PavelVasilchikov <91344984+PavelVasilchikov@users.noreply.github.com>
2025-01-25 19:26:24 +0300

    Обновление модели данных

--- SGS-Server/Models/CurrencyListModel.cs
+++ SGS-Server/Models/CurrencyListModel.cs
@@ -4,6 +4,24 @@ namespace SGS_Server.Models
 {
     public class CurrencyListModel
     {
-        
+        public class RootObject
+        {
+            public DateTime Date { get; set; }
+            public DateTime PreviousDate { get; set; }
+            public string PreviousURL { get; set; }
+            public DateTime Timestamp { get; set; }
+            public Dictionary<string, Currency> Valute { get; set; }
+        }
+
+        public class Currency
+        {
+            public string ID { get; set; }
+            public string NumCode { get; set; }
+            public string CharCode { get; set; }
+            public int Nominal { get; set; }
+            public string Name { get; set; }
+            public decimal Value { get; set; }
+            public decimal Previous { get; set; }
+        }
     }
 }
